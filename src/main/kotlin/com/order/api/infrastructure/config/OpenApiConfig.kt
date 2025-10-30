5b3dc4c5ccf724b2ede0f73a8e903f9a9285b541
alppinheiro <andre.l.pinheiro@hotmail.com>
2025-10-30 00:23:58 -0300

    adjuste doc swagger

--- src/main/kotlin/com/order/api/infrastructure/config/OpenApiConfig.kt
+++ src/main/kotlin/com/order/api/infrastructure/config/OpenApiConfig.kt
@@ -17,13 +17,13 @@ class OpenApiConfig {
         val info = Info()
             .title("ms-order-api")
             .version("0.0.1")
-            .description("API de gerenciamento de pedidos (Orders) - microserviço exemplo com Kotlin + Spring Boot")
-            .contact(Contact().name("Equipe de Dev").email("dev@example.com"))
+            .description("API de gerenciamento de pedidos (Pedidos) - microserviço exemplo com Kotlin + Spring Boot")
+            .contact(Contact().name("Equipe de Desenvolvimento").email("dev@example.com"))
             .license(License().name("MIT").url("https://opensource.org/licenses/MIT"))
 
-        val serverLocal = Server().url("http://localhost:8080").description("Local server")
+        val serverLocal = Server().url("http://localhost:8080").description("Servidor local")
 
-        val tagOrders = Tag().name("Orders").description("Operações relacionadas a pedidos (create/get)")
+        val tagOrders = Tag().name("Pedidos").description("Operações relacionadas a pedidos (criar/consultar)")
 
         return OpenAPI()
             .info(info)
